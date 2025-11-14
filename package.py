d796a0d0313b572cbe1baa09719924d930b49131
Aleks Berland <aleksberland@gmail.com>
2025-11-14 15:08:36 -0500

    Update version to 0.3.3 in package.py and pyproject.toml files for local_config addon, ensuring consistency across all related files.

--- package.py
+++ package.py
@@ -1,12 +1,10 @@
 name = "local_config"
 title = "Local Config"
-version = "0.1.22"
+version = "0.3.3"
 client_dir = "ayon_local_config"
 
 ayon_server_version = ">=1.1.2"
-ayon_required_addons = {
-    "core": ">0.3.2"
-}
+ayon_required_addons = {"core": ">0.3.2"}
 
 # Mapping of addon name to version requirements
 ayon_compatible_addons = {}
