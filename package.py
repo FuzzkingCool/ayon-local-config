2d4f8e9bd8bf3675fe6d300e262e192892a2d286
Aleks Berland <aleksberland@gmail.com>
2026-04-13 16:59:32 -0400

    Allow project to override addon version

--- package.py
+++ package.py
@@ -11,3 +11,6 @@ ayon_compatible_addons = {}
 
 # Mapping of addon name to version requirements
 ayon_optional_addons = {}
+
+# Allow project to override addon version
+project_can_override_addon_version = True
